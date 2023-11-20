ad582f5c2eba77e8779951b6b7457210c105e0d8
Pablo Ilari <ilari.pablo@gmail.com>
2023-11-20 13:30:49 -0300

    Corrección menor en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import RegistroForm from './components/RegistroForm/RegistroForm'
+import RegistroFormContainer from './components/RegistroFormContainer/RegistroFormContainer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <RegistroForm/>
+      <RegistroFormContainer/>
     </>
   )
 }
