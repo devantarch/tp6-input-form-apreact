08f6bfdfb15612769dac5e64b1cf483a89eb6364
SILVANA <silespindola87@gmail.com>
2023-11-21 20:47:12 -0300

    cambios

--- src/components/RegistroForm/RegistroForm.jsx
+++ src/components/RegistroForm/RegistroForm.jsx
@@ -1,54 +1,92 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const RegistroForm = () => {
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [correoElectronico, setCorreoElectronico] = useState('');
-  const [telefono, setTelefono] = useState('');
-  const [contrasena, setContrasena] = useState('');
-  const [confirmarContrasena, setConfirmarContrasena] = useState('');
+
+    const initialState = {
+        nombre: '',
+        apellido: '',
+        correoElectronico: '',
+        telefono: '',
+        contrasena: '',
+        confirmarContrasena: '',
+    };
+
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [correoElectronico, setCorreoElectronico] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [contrasena, setContrasena] = useState('');
+    const [confirmarContrasena, setConfirmarContrasena] = useState('');
+    const [formularioEnviado, setFormularioEnviado] = useState(false);
 
 
-  return (
-    
-    <form>
-      <label>Nombre:</label>
-      <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-      <br/>
-      
 
-      <label>Apellido:</label>
-      <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
-      <br/>
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-      <label>Correo Electrónico:</label>
-      <input
-        type="email"
-        value={correoElectronico}
-        onChange={(e) => setCorreoElectronico(e.target.value)}
-      />
-      <br/>
+        if (!nombre || !apellido || !correoElectronico || !telefono || !contrasena || !confirmarContrasena) {
+            alert('Por favor, complete todos los campos.');
+            return;
+        }
 
-      <label>Teléfono:</label>
-      <input type="tel" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
-      <br/>
+       if (contrasena !== confirmarContrasena) {
+            alert('Las contraseñas no coinciden.');
+            return;
+        }
 
-      <label>Contraseña:</label>
-      <input type="password" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
-      <br/>
+        setFormularioEnviado(true);
 
-      <label>Confirmar Contraseña:</label>
-      <input
-        type="password"
-        value={confirmarContrasena}
-        onChange={(e) => setConfirmarContrasena(e.target.value)}
-      />
-      <br/>
+        setNombre(initialState.nombre);
+        setApellido(initialState.apellido);
+        setCorreoElectronico(initialState.correoElectronico);
+        setTelefono(initialState.telefono);
+        setContrasena(initialState.contrasena);
+        setConfirmarContrasena(initialState.confirmarContrasena);
+    };
 
-      <button type="submit">Registrar</button>
-    </form>
-    
-  );
+    return (
+        <div className='registro-form-container'>
+           <form className='formulario' onSubmit={handleSubmit}>
+                <h1 className='title'> Formulario </h1>
+
+                <label> <p className='titulos-label'>Nombre:</p></label>
+                <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                <br />
+
+                <label><p className='titulos-label'>Apellido:</p></label>
+                <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+                <br />
+
+                <label><p className='titulos-label'>Correo Electrónico:</p></label>
+                <input
+                    type="email"
+                    value={correoElectronico}
+                    onChange={(e) => setCorreoElectronico(e.target.value)}
+                />
+                <br />
+
+                <label><p className='titulos-label'>Teléfono:</p></label>
+                <input type="tel" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                <br />
+
+                <label><p className='titulos-label'>Contraseña:</p></label>
+                <input type="password" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
+                <br />
+
+                <label><p className='titulos-label'>Confirmar Contraseña: </p></label>
+                <input
+                    type="password"
+                    value={confirmarContrasena}
+                    onChange={(e) => setConfirmarContrasena(e.target.value)}
+                />
+                <br />
+
+                <button className= 'btn' type="submit">Enviar</button>
+           {formularioEnviado && 
+                <p className='enviado'>Registro enviado. ¡Gracias!</p>
+            } </form>
+        </div>
+    );
 };
 
 export default RegistroForm;
